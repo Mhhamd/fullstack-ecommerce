ef3d1472ec6df74d5a6b8f988450b869bf3beba1
Muhammed Soliman <muhammedsoliman77@gmail.com>
2025-05-14 10:39:53 +0300

    refactor: improve responsive behavior of header and hero

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -21,97 +21,104 @@ function Header() {
     ];
 
     return (
-        <div className="w-full flex items-center justify-between py-7 px-10 bg-white border-b font-medium">
-            {/* Left section */}
-            <div className="flex-1 gap-6 items-center hidden lg:flex text-nowrap">
-                <ul className="flex gap-6">
-                    {navLinks.map((item) => (
-                        <NavLink
-                            key={item.name}
-                            to={item.link}
-                            className="text-sm hover:text-black/60 transition-all duration-300 tracking-wide"
-                        >
-                            {item.name}
-                            <hr className="w-0 mt-1 mx-auto border-none h-[1.5px] bg-gray-700 transition-all duration-300" />
-                        </NavLink>
-                    ))}
-                </ul>
+        <>
+            <div className="w-full bg-black py-3 text-center text-white uppercase text-[12px] sm:text-sm">
+                <p>free domestic shipping on orders over $50*</p>
             </div>
-
-            {/* Side menu for phones */}
-            <div
-                className={`fixed inset-0 z-[100] w-screen h-screen backdrop-blur-2xl bg-white/10 transition-all duration-300 ${
-                    isMenuOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
-                }`}
-            >
-                <div
-                    className={`bg-white absolute right-0 top-0 h-full w-4/5 sm:w-96 shadow-2xl flex flex-col items-start gap-10 justify-center px-10 transition-transform duration-300 ${
-                        isMenuOpen ? 'translate-x-0' : 'translate-x-full'
-                    }`}
-                >
-                    <div
-                        onClick={() => setIsMenuOpen(!isMenuOpen)}
-                        className="absolute top-10 right-10 text-2xl hover:opacity-50 transition-all duration-300 cursor-pointer"
-                    >
-                        <IoCloseSharp />
-                    </div>
-                    {navLinks.map((item) => {
-                        return (
+            <div className="w-full flex items-center justify-between py-7 px-5 sm:px-10 bg-white border-b font-medium">
+                {/* Left section */}
+                <div className="flex-1 gap-6 items-center hidden lg:flex text-nowrap">
+                    <ul className="flex gap-6">
+                        {navLinks.map((item) => (
                             <NavLink
                                 key={item.name}
                                 to={item.link}
-                                className="text-base flex items-center gap-4 hover:text-black/60 transition-all duration-300 tracking-wide"
-                                onClick={() => setIsMenuOpen(false)}
+                                className="text-sm hover:text-black/60 transition-all duration-300 tracking-wide"
                             >
-                                <span className="text-xl">{item.icon}</span>
-                                <span className="leading-none">
-                                    {item.name}
-                                    <hr className="w-0 mt-1 mx-auto border-none h-[1.5px] bg-gray-700 transition-all duration-300" />
-                                </span>
+                                {item.name}
+                                <hr className="w-0 mt-1 mx-auto border-none h-[1.5px] bg-gray-700 transition-all duration-300" />
                             </NavLink>
-                        );
-                    })}
+                        ))}
+                    </ul>
                 </div>
-            </div>
 
-            {/* Middle section */}
-            <div className="flex-none lg:flex-1 flex justify-center items-center hover:opacity-70 transition-all duration-300">
-                <Link to="/">
-                    <h1 className="text-4xl font-poppins font-semibold tracking-wider">
-                        MercadoX
-                    </h1>
-                </Link>
-            </div>
-
-            {/* Right section */}
-            <div className="flex-1 flex justify-end items-center gap-5 text-2xl">
-                <CiSearch className="hover:opacity-50 transition-all duration-300 cursor-pointer" />
-                <Link
-                    className="hover:opacity-50 transition-all duration-300"
-                    to={'/login'}
-                >
-                    <CiUser />
-                </Link>
-                <Link
-                    to={'/cart'}
-                    className="relative hover:opacity-50 transition-all duration-300 "
-                >
-                    <CiShoppingCart />
-                    <p
-                        className="absolute right-[-7px] bottom-[-5px] w-4
-                     text-center leading-4 bg-black text-white text-[8px] rounded-full"
-                    >
-                        0
-                    </p>
-                </Link>
+                {/* Side menu for phones */}
                 <div
-                    onClick={() => setIsMenuOpen(!isMenuOpen)}
-                    className="hover:opacity-50 transition-all duration-300 cursor-pointer block lg:hidden"
+                    className={`fixed inset-0 z-[100] w-screen h-screen backdrop-blur-2xl bg-white/10 transition-all duration-300 ${
+                        isMenuOpen
+                            ? 'opacity-100'
+                            : 'opacity-0 pointer-events-none'
+                    }`}
                 >
-                    <IoIosMenu />
+                    <div
+                        className={`bg-white absolute right-0 top-0 h-full w-4/5 sm:w-96 shadow-2xl flex flex-col items-start gap-10 justify-center px-10 transition-transform duration-300 ${
+                            isMenuOpen ? 'translate-x-0' : 'translate-x-full'
+                        }`}
+                    >
+                        <div
+                            onClick={() => setIsMenuOpen(!isMenuOpen)}
+                            className="absolute top-10 right-10 text-2xl hover:opacity-50 transition-all duration-300 cursor-pointer"
+                        >
+                            <IoCloseSharp />
+                        </div>
+                        {navLinks.map((item) => {
+                            return (
+                                <NavLink
+                                    key={item.name}
+                                    to={item.link}
+                                    className="text-base flex items-center gap-4 hover:text-black/60 transition-all duration-300 tracking-wide"
+                                    onClick={() => setIsMenuOpen(false)}
+                                >
+                                    <span className="text-xl">{item.icon}</span>
+                                    <span className="leading-none">
+                                        {item.name}
+                                        <hr className="w-0 mt-1 mx-auto border-none h-[1.5px] bg-gray-700 transition-all duration-300" />
+                                    </span>
+                                </NavLink>
+                            );
+                        })}
+                    </div>
+                </div>
+
+                {/* Middle section */}
+                <div className="flex-none lg:flex-1 flex justify-center items-center hover:opacity-70 transition-all duration-300">
+                    <Link to="/">
+                        <h1 className="text-2xl sm:text-4xl font-poppins font-semibold tracking-wider">
+                            MercadoX
+                        </h1>
+                    </Link>
+                </div>
+
+                {/* Right section */}
+                <div className="flex-1 flex justify-end items-center gap-5 text-2xl">
+                    <CiSearch className="hover:opacity-50 transition-all duration-300 cursor-pointer" />
+                    <Link
+                        className="hover:opacity-50 transition-all duration-300"
+                        to={'/login'}
+                    >
+                        <CiUser />
+                    </Link>
+                    <Link
+                        to={'/cart'}
+                        className="relative hover:opacity-50 transition-all duration-300 "
+                    >
+                        <CiShoppingCart />
+                        <p
+                            className="absolute right-[-7px] bottom-[-5px] w-4
+                     text-center leading-4 bg-black text-white text-[8px] rounded-full"
+                        >
+                            0
+                        </p>
+                    </Link>
+                    <div
+                        onClick={() => setIsMenuOpen(!isMenuOpen)}
+                        className="hover:opacity-50 transition-all duration-300 cursor-pointer block lg:hidden"
+                    >
+                        <IoIosMenu />
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -1,17 +1,19 @@
 function Hero() {
     const currentYear = new Date().getFullYear();
     return (
-        <div className="w-screen flex lg:flex-row flex-col overflow-hidden">
+        <div className="w-screen flex flex-col lg:flex-row overflow-hidden">
             {/* Left side */}
-            <div className="flex justify-center flex-col  bg-black left-0 text-white top-0   w-screen px-15 pt-20 lg:w-1/2">
+            <div className="flex justify-center items-center sm:items-start flex-col bg-black left-0 text-white top-0 px-20 pt-10 pb-10 sm:pt-20">
                 <p className="text-left text-sm tracking-wide ">
                     #20K new ware
                 </p>
-                <h1 className="text-4xl leading-25  uppercase font-semibold text-left md:text-6xl">
-                    All New Fashion <br /> in <br /> Summer {currentYear}
+                <h1 className="text-2xl sm:text-6xl leading-10 sm:leading-25  uppercase font-semibold text-center sm:text-left  ">
+                    All New Fashion <br className="hidden lg:block" /> in{' '}
+                    <br className="hidden sm:block" />
+                    Summer {currentYear}
                 </h1>
                 <button
-                    className="bg-white text-center text-black w-45 h-15 hover:bg-black hover:text-white transition-all duration-300 border border-white cursor-pointer mt-8
+                    className="bg-white text-center text-black w-45 h-15 hover:bg-black hover:text-white transition-all duration-300 border border-white cursor-pointer mt-8 active:bg-black active:text-white
                      "
                 >
                     SHOP NOW
@@ -20,7 +22,7 @@ function Hero() {
             <div>
                 {/* Hero image */}
                 <img
-                    className="object-center w-screen h-[110vh]    object-cover"
+                    className="object-center object-cover w-screen h-[80vh] sm:h-[110vh]"
                     src="/images/hero-bg.jpg"
                     alt="herobackground"
                 />
