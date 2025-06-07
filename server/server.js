e13bde8d654d9e4d73a0a9b425b4dc74267e40c3
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-06-07 11:34:55 +0300

    revert: remove async server startup to debug DB connection issues

--- server/server.js
+++ server/server.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -33,26 +24,12 @@ app.use((0, cors_1.default)({
         'X-Requested-With',
     ],
 }));
-const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        yield (0, mongodb_1.default)();
-        console.log('Database connected successfully');
-        // Routes
-        app.get('/', (req, res) => {
-            res.send('Server is working');
-        });
-        app.use('/api/protected', protectedRoute_1.default);
-        app.use('/api/product/', productRoute_1.default);
-        app.use('/api/user/', userRoute_1.default);
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    }
-    catch (error) {
-        console.error('Failed to start server:', error);
-        process.exit(1);
-    }
+(0, mongodb_1.default)();
+// Routes
+app.get('/', (req, res) => {
+    res.send('Server is working');
 });
-startServer();
+app.use('/api/protected', protectedRoute_1.default);
+app.use('/api/product/', productRoute_1.default);
+app.use('/api/user/', userRoute_1.default);
 exports.default = app;
