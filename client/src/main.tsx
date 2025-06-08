13552d84541270142decb891b043718f851eb0ce
Muhammed Soliman <152155286+Mhhamd@users.noreply.github.com>
2025-06-08 11:21:02 +0300

    add vercel json

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,7 +9,7 @@ import { FilterProvider } from './context/FilterProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <UserProvider>
                 <ProductProvider>
                     <FilterProvider>
